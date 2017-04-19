9a4140c374afa74cc8d0230fa95362304ce16ccb
Pawel Wolak <pawelwolakk@gmail.com>
2017-04-19 21:03:13 +0200

    Added flex styles for layout

--- app/src/layouts/CoreLayout/CoreLayout.jsx
+++ app/src/layouts/CoreLayout/CoreLayout.jsx
@@ -9,7 +9,7 @@ const CoreLayout = ({ children }) => (
     <div className="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
         <Header />
         <DrawerContainer />
-        <main className="mdl-layout__content mdl-color--grey-100">
+        <main className="mdl-layout__content mdl-color--grey-100 main">
             {children}
         </main>
     </div>
