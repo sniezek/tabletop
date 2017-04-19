4ea8ad6e8bae32cdea618b9b79f543c2c2a04171
Pawel Wolak <pawelwolakk@gmail.com>
2017-04-19 18:28:07 +0200

    Fixes in eslint

--- app/src/store/reducers.js
+++ app/src/store/reducers.js
@@ -1,19 +1,18 @@
-import { combineReducers } from 'redux'
-import locationReducer from './location'
+import { combineReducers } from "redux";
+import locationReducer from "./location";
 
-export const makeRootReducer = (asyncReducers) => {
-  return combineReducers({
+export const makeRootReducer = asyncReducers => combineReducers({
     location: locationReducer,
     user: (state = {}) => state,
     ...asyncReducers
-  })
-}
+});
 
 export const injectReducer = (store, { key, reducer }) => {
-  if (Object.hasOwnProperty.call(store.asyncReducers, key)) return
+    if (Object.hasOwnProperty.call(store.asyncReducers, key)) return;
 
-  store.asyncReducers[key] = reducer
-  store.replaceReducer(makeRootReducer(store.asyncReducers))
-}
+    /* eslint-disable no-param-reassign */
+    store.asyncReducers[key] = reducer;
+    store.replaceReducer(makeRootReducer(store.asyncReducers));
+};
 
-export default makeRootReducer
+export default makeRootReducer;
