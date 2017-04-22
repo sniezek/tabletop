66d191c794c5e425a18a5dd0df2e2fbc1f752f19
Katarzyna Konieczna <kmakonieczna@gmail.com>
2017-04-22 21:49:58 +0200

    Add TournamentRanking

--- server/src/main/java/tabletop/controllers/GameController.java
+++ server/src/main/java/tabletop/controllers/GameController.java
@@ -1,9 +1,11 @@
 package tabletop.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import tabletop.domain.game.Game;
+import tabletop.services.TournamentRankingService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +16,10 @@ import java.util.List;
 
 @RestController
 public class GameController {
+    @Autowired
+    private TournamentRankingService tournamentRankingService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/games")
-
     public List<Game> getGamesList() {
         List<Game> games = new ArrayList<>();
         games.add(Game.CHESS);
---
+++ server/src/main/java/tabletop/services/TournamentRankingService.java
@@ -0,0 +1,15 @@
+package tabletop.services;
+
+import tabletop.domain.game.Game;
+import tabletop.domain.ranking.TournamentRanking;
+import tabletop.domain.user.User;
+
+import java.util.List;
+
+/**
+ * Created by Katarzyna on 22.04.2017.
+ */
+public interface TournamentRankingService {
+
+    List<TournamentRanking> getRankingForGame(List<User> users, Game game);
+}
