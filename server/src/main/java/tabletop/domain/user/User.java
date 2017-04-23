e03c86e87e6c0b0216ae68d849a7f3c3f5eb8fc8
Olaf Sniezek <olafsniezek@gmail.com>
2017-04-23 22:56:46 +0200

    Added email address to User model

--- server/src/main/java/tabletop/domain/user/User.java
+++ server/src/main/java/tabletop/domain/user/User.java
@@ -1,11 +1,13 @@
 package tabletop.domain.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class User {
@@ -17,6 +19,9 @@ public class User {
     @NotEmpty
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+    @Email
+    @NotNull
+    private String email;
 
     public User() {
     }
@@ -35,6 +40,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
