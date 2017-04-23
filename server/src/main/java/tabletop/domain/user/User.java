cbfb06686fcdf6b1a01cb5ae8e90b40438ee5999
rafalgrm <rafal.grm@gmail.com>
2017-04-23 12:55:30 +0200

    Swiss pairing util class

--- server/src/main/java/tabletop/domain/user/User.java
+++ server/src/main/java/tabletop/domain/user/User.java
@@ -36,4 +36,9 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return getUsername();
+    }
 }
