55b86d6adaa6b8513f5eca88f938e13d7c00f894
MKULCZYK <mkulczyk93@gmail.com>
2017-04-23 20:15:16 +0200

    First attempt on registration

--- app/src/api.js
+++ app/src/api.js
@@ -5,9 +5,11 @@ class Api {
         this.login = this.login.bind(this);
         this.logout = this.logout.bind(this);
         this.user = this.user.bind(this);
+        this.register = this.user.bind(this);
     }
 
     user() {
+        console.log("TEST");
         return fetch(`${API_SERVER}/user`, {
             method: "POST",
             credentials: "include"
@@ -30,6 +32,18 @@ class Api {
         return fetch(`${API_SERVER}/logout`, {
             method: "POST",
             credentials: "include"
+        });
+    }
+
+    register({ username, password }) {
+        const body = new FormData();
+        body.append("username", username);
+        body.append("password", password);
+        console.log(body);
+        return fetch(`${API_SERVER}/users`, {
+            method: "POST",
+            credentials: "include",
+            body
         });
     }
 }
--- app/src/routes/Auth/components/RegisterView.jsx
+++ app/src/routes/Auth/components/RegisterView.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 import pure from "recompose/pure";
+import RegisterContainer from "../containers/RegisterContainer.jsx";
+
+const propTypes = {
+    router: PropTypes.object.isRequired
+};
 
 const enhance = pure;
 
-const RegisterView = () => (
-    <div>To be implemented...</div>
+const RegisterView = ({ router }) => (
+    <RegisterContainer
+        router={router}
+    />
 );
+
+RegisterView.propTypes = propTypes;
 
 export default enhance(RegisterView);
