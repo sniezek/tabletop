80fccae0e4a61d6df984a84b4ccb0d1ec4c792ec
Katarzyna Konieczna <kmakonieczna@gmail.com>
2017-05-05 21:19:13 +0200

    Change to QueryDSL in GameRankingRepository

--- server/src/main/java/tabletop/repositories/game/GameRankingRepository.java
+++ server/src/main/java/tabletop/repositories/game/GameRankingRepository.java
@@ -1,8 +1,24 @@
 package tabletop.repositories.game;
 
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 import tabletop.domain.ranking.GameRanking;
 
+import java.util.List;
+
 public interface GameRankingRepository extends CrudRepository<GameRanking, Long>, QueryDslPredicateExecutor<GameRanking>, GameRankingRepositoryCustom {
+    @Override
+    List<GameRanking> findAll(Predicate predicate);
+
+    @Override
+    List<GameRanking> findAll(Predicate predicate, Sort sort);
+
+    @Override
+    List<GameRanking> findAll(Predicate predicate, OrderSpecifier<?>... orders);
+
+    @Override
+    List<GameRanking> findAll(OrderSpecifier<?>... orders);
 }
--- server/src/main/java/tabletop/repositories/game/GameRankingRepositoryImpl.java
+++ server/src/main/java/tabletop/repositories/game/GameRankingRepositoryImpl.java
@@ -1,8 +1,13 @@
 package tabletop.repositories.game;
 
+import com.querydsl.core.types.Predicate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import tabletop.domain.game.Game;
 import tabletop.domain.ranking.GameRanking;
+import tabletop.domain.ranking.QGameRanking;
 import tabletop.domain.user.User;
 
 import javax.persistence.EntityManager;
@@ -67,6 +72,22 @@ public class GameRankingRepositoryImpl implements GameRankingRepositoryCustom {
             critQuery.where(rankingRoot.get("gameName").in(game.getName()));
             critQuery.orderBy(builder.desc(rankingRoot.get("points")));
             return entityManager.createQuery(critQuery).setMaxResults(20).getResultList();
+        }
+
+    }
+
+    private List<GameRanking> getRankingsForUsers2(Game game, List<User> users) {
+        if (users != null) {
+            List<Long> ids = users.stream().map(User::getId).collect(Collectors.toList());
+            QGameRanking gameRanking = QGameRanking.gameRanking;
+            Predicate predicate = gameRanking.userId.in(ids).and(gameRanking.gameName.eq(game.getName()));
+            return gameRankingRepository.findAll(predicate);
+        }
+        else {
+            QGameRanking gameRanking = QGameRanking.gameRanking;
+            Predicate predicate = gameRanking.gameName.eq(game.getName());
+            Pageable pageable = new PageRequest(0, 20, Sort.Direction.DESC, "points");
+            return gameRankingRepository.findAll(predicate, pageable).getContent();
         }
 
     }
