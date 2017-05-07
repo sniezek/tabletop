9c7e96a06bb383030aa48323db800b441db9e986
Pawel Wolak <pawelwolakk@gmail.com>
2017-05-07 09:22:00 +0200

    Fixed react warnings and dialog polyfill

---
+++ app/src/components/Dialog/index.js
@@ -0,0 +1,4 @@
+import { DialogActions, DialogTitle, DialogContent } from "react-mdl/lib/Dialog";
+import Dialog from "./Dialog.jsx";
+
+export { Dialog, DialogActions, DialogTitle, DialogContent };
