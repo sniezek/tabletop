e7b0e22d9d6fb97883ea0a188e6985840ce1e581
Pawel Wolak <pawelwolakk@gmail.com>
2017-05-08 12:49:23 +0200

    Unified drawer links format and added profile link for logged users

--- app/src/layouts/CoreLayout/containers/DrawerContainer.jsx
+++ app/src/layouts/CoreLayout/containers/DrawerContainer.jsx
@@ -1,7 +1,5 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
-import compose from "recompose/compose";
-import pure from "recompose/pure";
 import { connect } from "react-redux";
 import Drawer from "../components/Drawer";
 
@@ -17,12 +15,9 @@ const mapStateToProps = ({ user }) => ({
     user
 });
 
-const enhance = compose(
-    connect(mapStateToProps),
-    pure
-);
+const enhance = connect(mapStateToProps);
 
-const links = [{
+const defaultLinks = [{
     icon: "home",
     label: "Home",
     path: "/"
@@ -39,20 +34,62 @@ const links = [{
 const actions = [{
     label: "Login",
     icon: "lock_outline",
-    url: "/login"
+    path: "/login"
 }, {
     label: "Create account",
     icon: "person_add",
-    url: "/register"
+    path: "/register"
 }];
 
-const DrawerContainer = ({ user }) => (
-    <Drawer
-        user={user}
-        links={links}
-        actions={actions}
-    />
-);
+const profileLink = ({ name }) => ({
+    label: "My profile",
+    icon: "person",
+    path: `/users/${name}`
+});
+
+class DrawerContainer extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            links: defaultLinks
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const nextUser = nextProps.user;
+        const { user } = this.props;
+
+        const loggedOut = nextUser === null && user !== null;
+        const loggedIn = nextUser !== null && user === null;
+
+        if (loggedOut) {
+            this.setState({
+                links: defaultLinks
+            });
+        } else if (loggedIn) {
+            const links = [...defaultLinks];
+            links.splice(1, 0, profileLink(nextUser));
+
+            this.setState({
+                links
+            });
+        }
+    }
+
+    render() {
+        const { user } = this.props;
+        const { links } = this.state;
+
+        return (
+            <Drawer
+                user={user}
+                links={links}
+                actions={actions}
+            />
+        );
+    }
+}
 
 DrawerContainer.propTypes = propTypes;
 DrawerContainer.defaultProps = defaultProps;
