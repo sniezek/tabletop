10c4bdfd1e5448480b698d2d5490230b1ac67acb
Anna Szczepanska <anna.szczepanska.krk@gmail.com>
2017-05-08 23:04:11 +0200

    delete fake object data generator

--- app/src/routes/Tournaments/components/Match.jsx
+++ app/src/routes/Tournaments/components/Match.jsx
@@ -1,90 +1,92 @@
-import React, {PureComponent} from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import "./Match.scss";
-import {List, ListItem, ListItemAction, ListItemContent} from "react-mdl/lib/List";
-import {Checkbox} from "react-mdl/lib";
+import { List, ListItem, ListItemAction, ListItemContent } from "react-mdl/lib/List";
+import { Checkbox } from "react-mdl/lib";
 
 const propTypes = {
-  host: PropTypes.object.isRequired,
-  guest: PropTypes.object.isRequired,
-  winner: PropTypes.string.isRequired,
-  setWinner: PropTypes.func.isRequired,
-  updateNextRoundButton: PropTypes.func.isRequired
+    host: PropTypes.object.isRequired,
+    guest: PropTypes.object.isRequired,
+    winner: PropTypes.string.isRequired,
+    setWinner: PropTypes.func.isRequired,
+    updateNextRoundButton: PropTypes.func.isRequired
 };
 
 const defaultProps = {};
 
 class Match extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.winCallback = this.winCallback.bind(this)
-    this.winNokCallback = this.winNokCallback.bind(this)
-    this.win = this.win.bind(this)
-    this.setCheckboxes = this.setCheckboxes.bind(this)
-    this.setCheckboxes(this.props, null);
-  }
-
-  componentWillReceiveProps(nextProps){
-    this.setCheckboxes(nextProps, this.props)
-  }
-
-  setCheckboxes(propsNew, propsOld) {
-    if(propsOld == null  || propsNew !== propsOld) {
-      this.state = {
-        radiosDisabled: propsNew.winner != "0",
-        hostCheckboxValue: propsNew.winner == "1" ? 1 : 0,
-        guestCheckboxValue: propsNew.winner == "-1" ? 1 : 0
-      };
+    constructor(props) {
+        super(props);
+        this.winCallback = this.winCallback.bind(this);
+        this.winNokCallback = this.winNokCallback.bind(this);
+        this.win = this.win.bind(this);
+        this.setCheckboxes = this.setCheckboxes.bind(this);
+        this.setCheckboxes(this.props, null);
     }
-  }
 
-  win = (winner) => {
-    this.props.setWinner(winner, () => this.winCallback(winner === this.props.host), this.winNokCallback)
-  };
+    componentWillReceiveProps(nextProps) {
+        this.setCheckboxes(nextProps, this.props);
+    }
 
-  winCallback(hostWon) {
-    this.setState({
-      hostCheckboxValue: hostWon ? 1 : 0,
-      guestCheckboxValue: hostWon ? 0 : 1,
-      radiosDisabled: true
-    });
-    this.props.updateNextRoundButton()
-  }
+    setCheckboxes(propsNew, propsOld) {
+        if (propsOld == null || propsNew !== propsOld) {
+            this.state = {
+                radiosDisabled: propsNew.winner != "0",
+                hostCheckboxValue: propsNew.winner == "1" ? 1 : 0,
+                guestCheckboxValue: propsNew.winner == "-1" ? 1 : 0
+            };
+        }
+    }
 
-  winNokCallback() {
-    this.setState({
-      hostCheckboxValue: 0,
-      guestCheckboxValue: 0
-    });
-  }
+    win = (winner) => {
+        this.props.setWinner(winner, () => this.winCallback(winner === this.props.host), this.winNokCallback);
+    };
 
-  render() {
-    return (
-      <div>
-        <List style={{width: '300px'}}>
-          <ListItem>
-            <ListItemContent avatar="person">{this.props.host["username"]}</ListItemContent>
-            <ListItemAction>
-              <Checkbox
-                checked={this.state.hostCheckboxValue == 1}
-                onChange={(e) => this.win(this.props.host)}
-                disabled={this.state.radiosDisabled}/>
-            </ListItemAction>
-          </ListItem>
-          <ListItem>
-            <ListItemContent avatar="person">{this.props.guest["username"]}</ListItemContent>
-            <ListItemAction>
-              <Checkbox
-                checked={this.state.guestCheckboxValue == 1}
-                onChange={(e) => this.win(this.props.guest)}
-                disabled={this.state.radiosDisabled}/>
-            </ListItemAction>
-          </ListItem>
-        </List>
-        <br/>
-      </div>
-    )
-  }
+    winCallback(hostWon) {
+        this.setState({
+            hostCheckboxValue: hostWon ? 1 : 0,
+            guestCheckboxValue: hostWon ? 0 : 1,
+            radiosDisabled: true
+        });
+        this.props.updateNextRoundButton();
+    }
+
+    winNokCallback() {
+        this.setState({
+            hostCheckboxValue: 0,
+            guestCheckboxValue: 0
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <List style={{ width: "300px" }}>
+                    <ListItem>
+                        <ListItemContent avatar="person">{this.props.host.username}</ListItemContent>
+                        <ListItemAction>
+                            <Checkbox
+                                checked={this.state.hostCheckboxValue == 1}
+                                onChange={e => this.win(this.props.host)}
+                                disabled={this.state.radiosDisabled}
+                            />
+                        </ListItemAction>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemContent avatar="person">{this.props.guest.username}</ListItemContent>
+                        <ListItemAction>
+                            <Checkbox
+                                checked={this.state.guestCheckboxValue == 1}
+                                onChange={e => this.win(this.props.guest)}
+                                disabled={this.state.radiosDisabled}
+                            />
+                        </ListItemAction>
+                    </ListItem>
+                </List>
+                <br />
+            </div>
+        );
+    }
 }
 
 Match.propTypes = propTypes;
