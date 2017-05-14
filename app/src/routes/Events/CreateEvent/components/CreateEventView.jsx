a051047465297f36bad2124b2af68d9242530391
Pawel Wolak <pawelwolakk@gmail.com>
2017-05-14 22:59:13 +0200

    Added tabs setup

--- app/src/routes/Events/CreateEvent/components/CreateEventView.jsx
+++ app/src/routes/Events/CreateEvent/components/CreateEventView.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import pure from "recompose/pure";
-import { View, ViewHeader } from "../../../../components/View";
+import { Tabs, Tab } from "react-mdl/lib/Tabs";
+import { View, ViewHeader, ViewContent } from "../../../../components/View";
 import "./CreateEventView.scss";
 
 const enhance = pure;
 
 const CreateEventView = () => (
-    <View>
+    <View className="create-event">
         <ViewHeader
             title="Create new event"
         />
+        <ViewContent>
+            <div className="create-event__content mdl-shadow--2dp">
+                <Tabs activeTab={0}>
+                    <Tab>Details</Tab>
+                    <Tab>Sparrings</Tab>
+                    <Tab>Tournaments</Tab>
+                </Tabs>
+            </div>
+        </ViewContent>
     </View>
 );
 
