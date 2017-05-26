b8764bc906f8bfd28e124b1ef724850ac38e5921
Ewa Marczewska <ew.marczewska@gmail.com>
2017-05-26 22:19:32 +0200

    Statistics Container added

---
+++ app/src/routes/Games/Details/containers/StatisticsContainer.jsx
@@ -0,0 +1,43 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getGameStats } from "../../../../store/games";
+import GameStats from "../components/GameStats";
+
+const propTypes = {
+    gameStats: PropTypes.object,
+    router: PropTypes.object.isRequired
+};
+
+const defaultProps = {
+    gameStats: null
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+    getGameStats: dispatch(getGameStats(ownProps.router.params.name))
+});
+
+const mapStateToProps = state => ({
+    gameStats: state.games.gamesStats
+});
+const enhance = connect(mapStateToProps, mapDispatchToProps);
+
+class StatisticsContainer extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.gameStats = this.getGameStats.bind(this);
+    }
+
+    getGameStats() {
+    }
+
+    render() {
+        return (
+            <GameStats gameStats={this.props.gameStats} />);
+    }
+}
+
+StatisticsContainer.propTypes = propTypes;
+StatisticsContainer.defaultProps = defaultProps;
+
+export default enhance(StatisticsContainer);
