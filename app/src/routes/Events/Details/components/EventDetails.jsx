9487640ad85d8cb3be49398b9bdc9f1994b827a1
Pawel Wolak <pawelwolakk@gmail.com>
2017-05-29 14:51:38 +0200

    Added basic layout styling

--- app/src/routes/Events/Details/components/EventDetails.jsx
+++ app/src/routes/Events/Details/components/EventDetails.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import pure from "recompose/pure";
 import { View, ViewHeader, ViewContent } from "../../../../components/View";
+import "./EventDetails.scss";
 
 const propTypes = {
     name: PropTypes.string
@@ -13,13 +14,39 @@ const defaultProps = {
 
 const enhance = pure;
 
-const EventDetails = ({ name, id }) => (
-    <View>
+const EventDetails = ({ name, id, description }) => (
+    <View className="event">
         <ViewHeader
             title={name}
         />
         <ViewContent>
-            {id}
+            <div className="event__wrapper">
+                <div className="event__content">
+                    <main className="event__main">
+                        <div className="event-section mdl-shadow--2dp">
+                            <h3 className="event-section__title">Description</h3>
+                            {description}
+                        </div>
+                        <div className="event-section mdl-shadow--2dp">
+                            <h3 className="event-section__title">Tournaments</h3>
+                            {description}
+                        </div>
+                        <div className="event-section mdl-shadow--2dp">
+                            <h3 className="event-section__title">Sparrings</h3>
+                            {description}
+                        </div>
+                    </main>
+                    <aside className="event__sidebar">
+                        <div className="event-location">
+
+                        </div>
+                        <div className="event-section mdl-shadow--2dp">
+                            <h3 className="event-section__title">Participants</h3>
+                            {description}
+                        </div>
+                    </aside>
+                </div>
+            </div>
         </ViewContent>
     </View>
 );
