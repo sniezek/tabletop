78b0f1e8079685df3e7a2d8f208a32a6979e9db6
Pawel Wolak <pawelwolakk@gmail.com>
2017-05-29 16:21:17 +0200

    Improved component structure and styling

--- app/src/routes/Events/Details/components/EventDetails.jsx
+++ app/src/routes/Events/Details/components/EventDetails.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import pure from "recompose/pure";
 import { View, ViewHeader, ViewContent } from "../../../../components/View";
+import DetailsSection from "./DetailsSection.jsx";
+import Description from "./sections/Description.jsx";
+import Participants from "./sections/Participants.jsx";
+import Location from "./sections/Location.jsx";
 import "./EventDetails.scss";
 
 const propTypes = {
-    name: PropTypes.string
+    name: PropTypes.string,
+    description: PropTypes.string,
+    users: PropTypes.array,
+    location: PropTypes.object
 };
 
 const defaultProps = {
-    name: "Loading..."
+    name: "Loading...",
+    description: null,
+    users: null,
+    location: null
 };
 
 const enhance = pure;
 
-const EventDetails = ({ name, id, description }) => (
+const EventDetails = ({ name, description, users, location }) => (
     <View className="event">
         <ViewHeader
             title={name}
@@ -23,27 +33,25 @@ const EventDetails = ({ name, id, description }) => (
             <div className="event__wrapper">
                 <div className="event__content">
                     <main className="event__main">
-                        <div className="event-section mdl-shadow--2dp">
-                            <h3 className="event-section__title">Description</h3>
-                            {description}
-                        </div>
-                        <div className="event-section mdl-shadow--2dp">
-                            <h3 className="event-section__title">Tournaments</h3>
-                            {description}
-                        </div>
-                        <div className="event-section mdl-shadow--2dp">
-                            <h3 className="event-section__title">Sparrings</h3>
-                            {description}
-                        </div>
+                        <Description
+                            content={description}
+                        />
+                        <DetailsSection
+                            title="Tournaments"
+                            loading
+                        />
+                        <DetailsSection
+                            title="Sparrings"
+                            loading
+                        />
                     </main>
                     <aside className="event__sidebar">
-                        <div className="event-location">
-
-                        </div>
-                        <div className="event-section mdl-shadow--2dp">
-                            <h3 className="event-section__title">Participants</h3>
-                            {description}
-                        </div>
+                        <Location
+                            location={location}
+                        />
+                        <Participants
+                            list={users}
+                        />
                     </aside>
                 </div>
             </div>
