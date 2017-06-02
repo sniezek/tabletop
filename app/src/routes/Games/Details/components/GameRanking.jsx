21d50a35283a444cf8e8386a19e30ef18ae44d4e
Ewa Marczewska <ew.marczewska@gmail.com>
2017-06-02 21:19:22 +0200

    Ranking pager mock added

--- app/src/routes/Games/Details/components/GameRanking.jsx
+++ app/src/routes/Games/Details/components/GameRanking.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import pure from "recompose/pure";
-import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl/lib";
+import { List, ListItem, ListItemContent } from "react-mdl/lib";
+
 import "./GameRanking.scss";
 
 const propTypes = {
@@ -10,18 +11,16 @@ const propTypes = {
 
 const enhance = pure;
 
-export const GameRanking = ranking => (<Grid>
-    <Cell className="rankingContainer" shadow={1} col={8}>
-        <List className="width300">
-            {ranking.ranking.map(({ username, points }, index) =>
-                <ListItem twoLine key={username}>
-                    <h3 className="placeHeader">{index + 1}.</h3>
-                    <ListItemContent avatar="person" subtitle={points}>{username}</ListItemContent>
-                </ListItem>
+export const GameRanking = ranking => (
+    <List className="width300">
+        {ranking.ranking.map(({ username, points }, index) =>
+            <ListItem twoLine key={username}>
+                <h3 className="placeHeader">{index + 1}.</h3>
+                <ListItemContent avatar="person" subtitle={points}>{username}</ListItemContent>
+            </ListItem>
       )}
-        </List>
-    </Cell>
-</Grid>
+    </List>
+
     );
 
 GameRanking.propTypes = propTypes;
--- app/src/routes/Games/Details/containers/GameRankingContainer.jsx
+++ app/src/routes/Games/Details/containers/GameRankingContainer.jsx
@@ -1,6 +1,8 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Grid, Cell } from "react-mdl/lib";
+import Pager from "cs-react-pager";
 import { getGameRanking } from "../../../../store/games";
 import GameRanking from "../components/GameRanking";
 
@@ -26,14 +28,37 @@ class GameRankingContainer extends PureComponent {
     constructor(props) {
         super(props);
         this.getGameRanking = this.getGameRanking.bind(this);
+        this.state = {
+            current: 1
+        };
+        this.gotoPage = this.gotoPage.bind(this);
     }
 
     getGameRanking() {
     }
 
+    gotoPage(page) {
+        this.setState({
+            current: page
+        });
+    }
+
     render() {
         return (
-            <GameRanking ranking={this.props.gameRankingList} />);
+            <div>
+                <Grid>
+                    <Cell className="rankingContainer" shadow={1} col={8}>
+                        <GameRanking ranking={this.props.gameRankingList} />
+                        <Pager
+                            total={300}
+                            current={this.state.current}
+                            gotoPage={this.gotoPage}
+                            locale="en"
+                        />
+                    </Cell>
+                </Grid>
+            </div>
+        );
     }
 }
 
