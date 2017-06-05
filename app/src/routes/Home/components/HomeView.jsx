779179ba289840f76eefe092cc9550884a8fd304
Olaf Sniezek <olafsniezek@gmail.com>
2017-06-05 15:07:15 +0200

    Home page

--- app/src/routes/Home/components/HomeView.jsx
+++ app/src/routes/Home/components/HomeView.jsx
@@ -11,7 +11,7 @@ export const HomeView = () => (
             title="Home"
         />
         <ViewContent>
-            <div style={{ padding: "32px" }}>To be implemented...</div>
+            <div style={{ padding: "32px" }}>Tabletop is a website where users can organise and join tabletop games events. <br /><br />Matches can be either sparrings or tournaments. <br /><br />Tournament scores are submitted by events' organisers, therefore each registered game has a ranking of players. <br /><br />Users have a variety of statistics and they can get achievements for being active players.</div>
         </ViewContent>
     </View>
 );
