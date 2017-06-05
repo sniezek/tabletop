48e5054131d4c7aeff844d7ed42effc96a5d54d4
Olaf Sniezek <olafsniezek@gmail.com>
2017-06-05 13:44:01 +0200

    Pending discarded fix

--- server/src/main/java/tabletop/controllers/event/dto/EventSparringDto.java
+++ server/src/main/java/tabletop/controllers/event/dto/EventSparringDto.java
@@ -8,8 +8,8 @@ import java.util.Optional;
 class EventSparringDto extends EventMatchDto {
     private final String gameName;
 
-    EventSparringDto(Sparring sparring, Optional<User> user) {
-        super(sparring.getId(), sparring.getStartDate(), sparring.getEndDate(), sparring.getUsers(), sparring.getGame(), sparring.getMinPlayers(), sparring.getMaxPlayers(), sparring.getPending(), sparring.getDiscarded(), user);
+    EventSparringDto(Sparring sparring, Optional<User> user, User organiser) {
+        super(sparring.getId(), sparring.getStartDate(), sparring.getEndDate(), sparring.getUsers(), sparring.getGame(), sparring.getMinPlayers(), sparring.getMaxPlayers(), sparring.getPending(), sparring.getDiscarded(), user, organiser);
         this.gameName = sparring.getGameName();
     }
 
